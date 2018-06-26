2ac01d2cc58ac858fe3b5f2d3f0671277735c52a
t-adghos <40592111+t-adghos@users.noreply.github.com>
2018-06-26 13:09:42 +0530

    Update Flaskapp.py

--- Flaskapp.py
+++ Flaskapp.py
@@ -1,23 +1,9 @@
-import pyodbc,decimal
-from flask import Flask, jsonify
-from flask import abort
-from flask import make_response
-
+from flask import Flask
 app = Flask(__name__)
 
-
-@app.route('/<string:task_id>', methods=['GET'])
-def get_task(task_id):
-    print task_id
-
-
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Incorrect query'}), 404)
-
-
-
-
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run()
